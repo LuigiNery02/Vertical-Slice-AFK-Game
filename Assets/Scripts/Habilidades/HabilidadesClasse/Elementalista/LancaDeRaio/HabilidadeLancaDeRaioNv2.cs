5f556729e5f8c96baae23aee0434f72a0fd7e994
LuigiNery02 <101671365+LuigiNery02@users.noreply.github.com>
2025-07-29 16:39:17 -0300

    Implementação Habilidades Elementalista Parte 2
    -implementação da habilidade lança de raio
    -implementação da habilidade nevasca
    -implementação da habilidade meteoro
    -implementação da habilidade trovoada

--- Assets/Scripts/Habilidades/HabilidadesClasse/Elementalista/LancaDeRaio/HabilidadeLancaDeRaioNv2.cs
+++ Assets/Scripts/Habilidades/HabilidadesClasse/Elementalista/LancaDeRaio/HabilidadeLancaDeRaioNv2.cs
@@ -1,6 +1,88 @@
+using System.Collections;
 using UnityEngine;
 
-public class HabilidadeLancaDeRaioNv2 : MonoBehaviour
+[CreateAssetMenu(menuName = "Habilidades/Ativa/Classe/Elementalista/Lança de Raio/Nv2")]
+public class HabilidadeLancaDeRaioNv2 : HabilidadeAtiva
 {
+    [Header("Configurações Habilidade")]
+    [SerializeField]
+    private float multiplicadorDeAtaque = 3;
+    [SerializeField]
+    private GameObject hit;
+    [SerializeField]
+    private int quantidadeDeLancas = 2;
+    [SerializeField]
+    private float tempoEntreLancas = 0.15f;
+    public GameObject vfx;
 
+    [Header("Configurações Hit")]
+    [SerializeField]
+    public float velocidadeDeMovimento = 20;
+
+    public override void AtivarEfeito(IAPersonagemBase personagem)
+    {
+        if (personagem.podeAtivarEfeitoHabilidadeAtivaClasse)
+        {
+            if (base.ChecarAtivacao(personagem) && base.ChecarRuna(personagem, nivel))
+            {
+                personagem.GastarSP(custoDeMana);
+
+                base.ChecarCastingHabilidade1(personagem, () =>
+                {
+                    float dano = personagem._dano * multiplicadorDeAtaque;
+
+                    //movimento especial
+                    personagem.movimentoEspecial = "LancaElemento";
+                    personagem.VerificarComportamento("movimentoEspecial");
+
+                    personagem.StartCoroutine(EsperarTempoParaInstanciarHit(personagem, dano));
+                });
+            }
+        }
+    }
+
+    public override void RemoverEfeito(IAPersonagemBase personagem)
+    {
+        base.RemoverEfeito(personagem);
+    }
+
+    IEnumerator EsperarTempoParaInstanciarHit(IAPersonagemBase personagem, float dano)
+    {
+        Transform posicao = null;
+
+        yield return new WaitForSeconds(0.5f);
+        //instancia o hit
+        if (personagem.GetComponentInChildren<AtivarArmaPersonagem>() != null)
+        {
+            posicao = personagem.GetComponentInChildren<AtivarArmaPersonagem>().maoBone;
+        }
+        else
+        {
+            Vector3 posicaoValor = personagem.transform.position;
+            posicaoValor.y += 1.5f;
+            posicao.position = posicaoValor;
+        }
+
+        for (int i = 0; i < quantidadeDeLancas; i++)
+        {
+            GameObject lancaGelo = Instantiate(hit, posicao.position, hit.transform.rotation);
+
+            HitAtaqueEspecial3Personagem hitComponente = lancaGelo.GetComponent<HitAtaqueEspecial3Personagem>();
+            if (hitComponente != null)
+            {
+                hitComponente._personagemPai = personagem;
+                hitComponente.dano = dano;
+                hitComponente.elemento = "raio";
+                hitComponente.vfxImpacto = vfx;
+                hitComponente.MoverAteAlvo(personagem._personagemAlvo.transform, velocidadeDeMovimento);
+            }
+
+            if (i < quantidadeDeLancas - 1)
+            {
+                yield return new WaitForSeconds(0.3f);
+            }
+        }
+
+        RemoverEfeito(personagem);
+    }
 }
--- Assets/Scripts/Habilidades/HabilidadesClasse/Elementalista/Meteoro/HabilidadeMeteoroNv1.cs
+++ Assets/Scripts/Habilidades/HabilidadesClasse/Elementalista/Meteoro/HabilidadeMeteoroNv1.cs
@@ -1,6 +1,71 @@
+using System.Collections;
 using UnityEngine;
 
-public class HabilidadeMeteoroNv1 : MonoBehaviour
+[CreateAssetMenu(menuName = "Habilidades/Ativa/Classe/Elementalista/Meteoro/Nv1")]
+public class HabilidadeMeteoroNv1 : HabilidadeAtiva
 {
+    [Header("Configurações Habilidade")]
+    [SerializeField]
+    private float multiplicadorDeAtaque = 3.5f;
+    [SerializeField]
+    private float raioVfx = 10;
+    [SerializeField]
+    private float tempoDeVfx = 3;
+    public GameObject vfx;
 
+    public override void AtivarEfeito(IAPersonagemBase personagem)
+    {
+        if (personagem.podeAtivarEfeitoHabilidadeAtivaClasse)
+        {
+            if (base.ChecarAtivacao(personagem) && base.ChecarRuna(personagem, nivel))
+            {
+                base.ChecarCastingHabilidade1(personagem, () =>
+                {
+                    //movimento especial
+                    personagem.movimentoEspecial = "LancaElemento";
+                    personagem.VerificarComportamento("movimentoEspecial");
+
+                    personagem.StartCoroutine(Nevasca(personagem));
+                });
+            }
+        }
+    }
+
+    public override void RemoverEfeito(IAPersonagemBase personagem)
+    {
+        personagem.GerenciarVFXHabilidade(1, false);
+        base.RemoverEfeito(personagem);
+    }
+
+    IEnumerator Nevasca(IAPersonagemBase personagem)
+    {
+        float dano = personagem._dano * multiplicadorDeAtaque;
+
+        Vector3 posicaoAlvo = personagem._personagemAlvo.transform.position;
+
+        GameObject vfxInstanciado = GameObject.Instantiate(vfx, posicaoAlvo, Quaternion.identity);
+        personagem.vfxHabilidadeAtivaClasse = vfxInstanciado;
+        vfxInstanciado.transform.localScale = (Vector3.one * raioVfx) / 9;
+
+        Collider[] colliders = Physics.OverlapSphere(posicaoAlvo, raioVfx / 2f);
+
+        foreach (var collider in colliders)
+        {
+            IAPersonagemBase inimigo = collider.GetComponent<IAPersonagemBase>();
+
+            if (inimigo != null && inimigo.controlador != personagem.controlador && inimigo._comportamento != EstadoDoPersonagem.MORTO)
+            {
+                inimigo.SofrerDano(dano, false, personagem);
+                if (personagem.cargasDeGelo != 0 || personagem.cargasDeFogo != 0 || personagem.cargasDeRaio != 0)
+                {
+                    inimigo.CausarEfeitoCargasElementais(personagem.cargasDeGelo, personagem.cargasDeFogo, personagem.cargasDeRaio, personagem._dano);
+                }
+                personagem.AtualizarCargasElementais("fogo");
+            }
+        }
+
+        yield return new WaitForSeconds(tempoDeVfx);
+
+        RemoverEfeito(personagem);
+    }
 }
