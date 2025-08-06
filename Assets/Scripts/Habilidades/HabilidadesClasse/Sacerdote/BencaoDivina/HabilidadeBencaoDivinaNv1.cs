d42a90c6a0c6fa6ee6030ce4e7363c5370204d1d
LuigiNery02 <101671365+LuigiNery02@users.noreply.github.com>
2025-08-06 16:19:13 -0300

    Implementação Habilidades Sacerdote Parte 1
    -implementação da habilidade benção divina
    -implementação da habilidade grande benção

--- Assets/Scripts/Habilidades/HabilidadesClasse/Sacerdote/BencaoDivina/HabilidadeBencaoDivinaNv1.cs
+++ Assets/Scripts/Habilidades/HabilidadesClasse/Sacerdote/BencaoDivina/HabilidadeBencaoDivinaNv1.cs
@@ -1,15 +1,76 @@
+using System.Collections;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Habilidades/Ativa/Classe/Sacerdote/Benção Divina/Nv1")]
 public class HabilidadeBencaoDivinaNv1 : HabilidadeAtiva
 {
+    [Header("Configurações Habilidade")]
+    [SerializeField]
+    private float multiplicadorCura = 1.5f;
+    public GameObject vfx;
+    [SerializeField]
+    private GameObject vfxAliado;
+    [SerializeField]
+    private float tempoDeVfxAliado = 1;
     public override void AtivarEfeito(IAPersonagemBase personagem)
     {
+        if (personagem.podeAtivarEfeitoHabilidadeAtivaClasse)
+        {
+            if (base.ChecarAtivacao(personagem) && base.ChecarRuna(personagem, nivel))
+            {
+                personagem.GastarSP(custoDeMana);
 
+                base.ChecarCastingHabilidade1(personagem, () =>
+                {
+                    if (personagem.vfxHabilidadeAtivaClasse == null)
+                    {
+                        GameObject vfxInstanciado = GameObject.Instantiate(vfx, personagem.transform.position + Vector3.zero, personagem.transform.rotation, personagem.transform);
+                        personagem.vfxHabilidadeAtivaClasse = vfxInstanciado;
+                    }
+                    else
+                    {
+                        personagem.GerenciarVFXHabilidade(1, true);
+                    }
+
+                    IAPersonagemBase aliadoComMenorHP = null;
+                    float menorHP = 9999999999999;
+
+                    foreach (IAPersonagemBase aliado in GameObject.FindObjectsOfType<IAPersonagemBase>())
+                    {
+                        if (aliado != null && aliado.controlador == personagem.controlador && aliado._comportamento != EstadoDoPersonagem.MORTO && aliado != personagem)
+                        {
+                            float hpAtual = aliado.hpAtual;
+
+                            if (hpAtual < menorHP)
+                            {
+                                menorHP = hpAtual;
+                                aliadoComMenorHP = aliado;
+                            }
+                        }
+                    }
+
+                    float cura = aliadoComMenorHP._dano * multiplicadorCura;
+                    personagem.CurarAliado(aliadoComMenorHP, cura);
+
+                    GameObject vfxAliadoInstanciado = GameObject.Instantiate(vfxAliado, aliadoComMenorHP.transform.position + Vector3.zero, aliadoComMenorHP.transform.rotation, aliadoComMenorHP.transform);
+                    personagem.StartCoroutine(EsperarVFXAliado(personagem, vfxAliadoInstanciado));
+
+                    base.ChecarEfeitosAoAtivarHabilidade(personagem);
+                });
+            }
+        }
     }
 
     public override void RemoverEfeito(IAPersonagemBase personagem)
     {
+        personagem.GerenciarVFXHabilidade(1, false);
+        base.RemoverEfeito(personagem);
+    }
 
+    IEnumerator EsperarVFXAliado(IAPersonagemBase personagem, GameObject vfx)
+    {
+        yield return new WaitForSeconds(tempoDeVfxAliado);
+        Destroy(vfx);
+        RemoverEfeito(personagem);
     }
 }
--- Assets/Scripts/Habilidades/HabilidadesClasse/Sacerdote/GrandeBencao/HabilidadeGrandeBencaoNv1.cs
+++ Assets/Scripts/Habilidades/HabilidadesClasse/Sacerdote/GrandeBencao/HabilidadeGrandeBencaoNv1.cs
@@ -1,15 +1,71 @@
+using System.Collections;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Habilidades/Ativa/Classe/Sacerdote/Grande Benção/Nv1")]
 public class HabilidadeGrandeBencaoNv1 : HabilidadeAtiva
 {
+    [Header("Configurações Habilidade")]
+    [SerializeField]
+    private float multiplicadorCura = 1f;
+    [SerializeField]
+    private int numeroDeCuras = 1;
+    public GameObject vfx;
+    [SerializeField]
+    private GameObject vfxAliado;
+    [SerializeField]
+    private float tempoDeVfxAliado = 1;
     public override void AtivarEfeito(IAPersonagemBase personagem)
     {
+        if (personagem.podeAtivarEfeitoHabilidadeAtivaClasse)
+        {
+            if (base.ChecarAtivacao(personagem) && base.ChecarRuna(personagem, nivel))
+            {
+                personagem.GastarSP(custoDeMana);
 
+                base.ChecarCastingHabilidade1(personagem, () =>
+                {
+                    float cura = personagem._dano * multiplicadorCura;
+
+                    if (personagem.vfxHabilidadeAtivaClasse == null)
+                    {
+                        GameObject vfxInstanciado = GameObject.Instantiate(vfx, personagem.transform.position + Vector3.zero, personagem.transform.rotation, personagem.transform);
+                        personagem.vfxHabilidadeAtivaClasse = vfxInstanciado;
+                    }
+                    else
+                    {
+                        personagem.GerenciarVFXHabilidade(1, true);
+                    }
+
+                    foreach (IAPersonagemBase aliado in GameObject.FindObjectsOfType<IAPersonagemBase>())
+                    {
+                        if (aliado != null && aliado.controlador == personagem.controlador && aliado._comportamento != EstadoDoPersonagem.MORTO && aliado != personagem)
+                        {
+                            for(int i = 0; i < numeroDeCuras; i++)
+                            {
+                                personagem.CurarAliado(aliado, cura);
+
+                                GameObject vfxAliadoInstanciado = GameObject.Instantiate(vfxAliado, aliado.transform.position + Vector3.zero, aliado.transform.rotation, aliado.transform);
+                                personagem.StartCoroutine(EsperarVFXAliado(personagem, vfxAliadoInstanciado));
+                            }
+                        }
+                    }
+
+                    base.ChecarEfeitosAoAtivarHabilidade(personagem);
+                });
+            }
+        }
     }
 
     public override void RemoverEfeito(IAPersonagemBase personagem)
     {
+        personagem.GerenciarVFXHabilidade(1, false);
+        base.RemoverEfeito(personagem);
+    }
 
+    IEnumerator EsperarVFXAliado(IAPersonagemBase personagem, GameObject vfx)
+    {
+        yield return new WaitForSeconds(tempoDeVfxAliado);
+        Destroy(vfx);
+        RemoverEfeito(personagem);
     }
 }
